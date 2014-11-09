e6c0a222f9c0adbf49386200efed7e13906ea38a
wil <william.barsse@gmail.com>
2014-11-09 18:20:42 +0100

    Added scapy extension

---
+++ toysm/ext/scapy_ext.py
@@ -0,0 +1,169 @@
+################################################################################
+#
+# Copyright 2014 William Barsse
+#
+################################################################################
+#
+# This file is part of ToySM Extensions.
+#
+# ToySM Extensions is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ToySM Extensions is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+# pylint: disable=invalid-name
+
+'''Classes intended to simplify using ToySM with Scapy:
+   - PacketTransition: allows defining transitions between States based
+                       on events that are scapy Packet templates.
+   - SMBox: A Scapy pipe box that uses the high/low inputs to post
+   	    events to a state machine.
+'''
+
+from inspect import isclass
+from scapy.packet import Packet, NoPayload
+#from scapy.pipetools import AutoSource
+from toysm import Transition, public
+from toysm.public import public
+
+@public
+class ForbidPayload(Packet):
+    '''Used in the context of matching a packet template.
+       When used as the payload of another packet in a template,
+       only packets that don't have a payload will match.
+    '''
+    # pylint: disable=no-init, too-few-public-methods
+    pass
+
+
+@public
+def match_packet(template, packet):
+    '''returns True if a scapy Packet matches a template.
+       Matching occurs if:
+       - packet is of the same class or a subclass of template
+         or if a sublayer of packet meets this criteria.
+       - and for each field defined in the template the packet has a field of
+         equal value.
+       - for template field values that are Packets themselves, the
+         corresponding field in packet will be recursively matched using
+         match_packet
+       - the payloads of template and packet match according to match_packet.
+         if template has no payload and the previous criteria are met the
+         packet is considered to have matched the template.
+    '''
+    def inner_match_packet(template, packet):
+        '''Actual matching is done in this function, the outer function
+           only ensures iteration over template instances.
+        '''
+        tcls = type(template)
+        while True:
+            if isinstance(packet, tcls):
+                break
+            else:
+                if isinstance(packet.payload, NoPayload):
+                    return False
+                else:
+                    packet = packet.payload
+        for fname, v_template in template.fields.items():
+            try:
+                v_packet = getattr(packet, fname)
+            except AttributeError:
+                return False
+            if isinstance(v_template, Packet):
+                if inner_match_packet(v_template, v_packet):
+                    continue
+            elif v_packet == v_template:
+                continue
+            return False
+        if isinstance(template.payload, NoPayload):
+            return True
+        elif isinstance(template.payload, ForbidPayload):
+            return isinstance(packet.payload, NoPayload)
+        else:
+            return inner_match_packet(template.payload, packet.payload)
+
+    if isclass(template):
+        template = template()
+    for t in template:
+        if inner_match_packet(t, packet):
+            return True
+    return False
+
+@public
+class PacketTransition(Transition):
+    '''Transition that triggers when the event matches a Scapy Packet
+       template.
+
+       A PacketTransition is created with a Scapy Packet Template:
+
+       PacketTransition(IP()/UDP(dport=53))
+         would create a transition that matches any IP packet (regardless
+         of src/dst addresses) contains a UDP layer destined to port 53.
+
+       PacketTransition(IP(dst=['1.2.3.4', '5.6.7.8'])/TCP())
+         this transition would trigger on any IP/TCP packet sent to
+         either 1.2.3.4 or 5.6.7.8.
+         Note: the following packet would also match
+               Ether()/IP(dst='1.2.3.4')/TCP()
+    '''
+
+    @classmethod
+    def ctor_accepts(cls, value, **_):
+        '''Register constructor as supporting any Packet value.'''
+        return isinstance(value, Packet)
+
+    def __init__(self, template, desc=None, **kargs):
+        desc = repr(template) + ('/%s' % desc if desc else '')
+        super(PacketTransition, self).__init__(desc=desc, **kargs)
+        self.template = template
+
+    def is_triggered(self, evt):
+        return match_packet(self.template, evt)
+
+
+#@public
+#class SMBox(StateMachine, AutoSource):
+#    def __init__(self, *args, **kargs):
+#        StateMachine.__init__(self, *args, **kargs)
+#        AutoSource.__init__(self,
+#                            **{k:v for k,v in karg.items if k in {'name'}})
+#
+#    def push(self, msg):
+#        self.post(self.convert(msg))
+#
+#    def high_push(self, msg):
+#        self.post(self.high_convert(msg))
+#
+#    def convert(self, msg):
+#        '''Converts a message received on the low input into an
+#           event (to be posted to the SM).'''
+#        return msg
+#
+#    def high_convert(self, msg):
+#        '''Converts a message received on the high input into an
+#           event (to be posted to the SM).'''
+#        return msg
+#
+#    def send(self, msg):
+#        '''Publishes a message on the Box's low output.'''
+#        self._gen_data(msg)
+#
+#    def high_send(self, msg):
+#        '''Publishes a message on the Box's high output.'''
+#        self._gen_hight_data(msg)
+#
+#    def stop(self):
+#        StateMachine.stop(self)
+#        self.exhausted = True
+
+# vim:expandtab:sw=4:sts=4
---
+++ toysm/ext/tests/test_scapy.py
@@ -0,0 +1,93 @@
+################################################################################
+#
+# Copyright 2014 William Barsse 
+#
+################################################################################
+#
+# This file is part of ToySM Extensions.
+# 
+# ToySM Extensions is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ToySM Extensions is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+import sys
+import unittest
+import logging
+
+LOG_LEVEL = logging.INFO
+#LOG_LEVEL = logging.DEBUG
+
+logging.basicConfig(level=LOG_LEVEL)
+
+SCAPY_COMPAT = sys.version_info.major < 3
+
+if SCAPY_COMPAT:
+    from scapy.all import *
+    from toysm.ext.scapy_ext import *
+
+@unittest.skipIf(not SCAPY_COMPAT,
+                 'Scapy is Python2 only, for now at least')
+class TestPacketTransition(unittest.TestCase):
+    def test_match_packet_class_tmpl(self):
+        self.assertTrue(
+            match_packet(IP, IP(str(IP()))))
+
+    def test_match_packet_field_match(self):
+        self.assertTrue(
+            match_packet(IP(), IP(str(IP()))))
+        self.assertFalse(
+            match_packet(IP(src='1.2.3.4'), 
+                         IP(str(IP(src='2.3.4.5')))))
+        self.assertFalse(
+            match_packet(IP(src='1.2.3.4'), 
+                         IP(str(IP(dst='1.2.3.4')))))
+
+    def test_match_packet_find_layer(self):
+        self.assertTrue(
+            match_packet(IP(src='1.2.3.4'), 
+                         Ether(str(Ether()/IP(src='1.2.3.4')/UDP()))))
+
+    def test_match_packet_skip_layer(self):
+        self.assertTrue(
+            match_packet(IP(src='1.2.3.4')/DNS(id=1), 
+                         Ether(str(Ether()/IP(src='1.2.3.4')/UDP()/DNS(id=1)))))
+        self.assertFalse(
+            match_packet(IP(src='4.5.6.7')/DNS(id=1), 
+                         Ether(str(Ether()/IP(src='1.2.3.4')/UDP()/DNS(id=1)))))
+        self.assertFalse(
+            match_packet(IP(src='1.2.3.4')/DNS(id=2), 
+                         Ether(str(Ether()/IP(src='1.2.3.4')/UDP()/DNS(id=1)))))
+
+    def test_match_forbid_pld(self):
+        self.assertTrue(
+            match_packet(IP(src='1.2.3.4')/ForbidPayload(), 
+                         Ether(str(Ether()/IP(src='1.2.3.4')))))
+        self.assertFalse(
+            match_packet(IP(src='1.2.3.4')/ForbidPayload(), 
+                         Ether(str(Ether()/IP(src='1.2.3.4')/UDP()))))
+
+    def test_match_missing_pld(self):
+        self.assertFalse(
+            match_packet(IP()/UDP()/DNS(),
+                         Ether()/IP()))
+
+    def test_match_list(self):
+        self.assertTrue(
+            match_packet(IP(src='1.2.3.4/30'), 
+                         Ether(str(Ether()/IP(src='1.2.3.4')))))
+        self.assertTrue(
+            match_packet(IP(src='1.2.3.4/30'), 
+                         Ether(str(Ether()/IP(src='1.2.3.5')))))
+
+# vim:expandtab:sw=4:sts=4
