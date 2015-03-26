e2f857822c0ddf8b02ac588d2eed81210df653a6
wil <william.barsse@gmail.com>
2015-03-26 21:14:52 +0100

    Fix up GPL header to replace Foobar with ToySM :-)

--- toysm/event_queue.py
+++ toysm/event_queue.py
@@ -6,7 +6,7 @@
 #
 # This file is part of ToySM.
 #
-# ToySM Extensions is free software: you can redistribute it and/or modify
+# ToySM is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
@@ -17,7 +17,7 @@
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with ToySM.  If not, see <http://www.gnu.org/licenses/>.
 #
 ################################################################################
 
--- toysm/ext/__init__.py
+++ toysm/ext/__init__.py
@@ -17,6 +17,6 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with ToySM.  If not, see <http://www.gnu.org/licenses/>.
 #
 ################################################################################
